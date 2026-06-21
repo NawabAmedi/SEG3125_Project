b1cc81e0db2e07ff71a422dc3894feb9a996aaf3
NawabAmedi <nawabamedi@gmail.com>
2026-06-20 20:05:34 -0400

    Update Design2.js

--- Design2.js
+++ Design2.js
@@ -1,1 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Design2 from "./Design2.jsx";
 
+const root = ReactDOM.createRoot(
+    document.getElementById("root")
+);
+
+root.render(
+    <React.StrictMode>
+        <Design2 />
+    </React.StrictMode>
+);
